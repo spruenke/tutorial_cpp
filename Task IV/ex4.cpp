92f48a50d7c9d9866a21d139952280c45cee3fd2
Erin Spruenken <48289831+spruenke@users.noreply.github.com>
2025-03-24 13:45:00 +0100

    Add files via upload

--- Task IV/ex4.cpp
+++ Task IV/ex4.cpp
@@ -1,16 +1,16 @@
-#include<RcppArmadillo>
-// [[Rcpp::depends(RcppArmadillo)]]
+#include <RcppArmadillo.h>
+ // [[Rcpp::depends(RcppArmadillo)]]
 
-// [[Rcpp::export()]]
-arma::vec tapply_cpp(const arma::vec x, const arma::vec grp){
-   arma::vec grps = arma::unique(grp);
-   int d = grps.n_elem;
-   arma::vec results(d);
-   
-   for(int i = 0; i < d; i++){
-      double current_grp = grps(i);
-      arma::uvec ind = arma::find(grp == current_grp);
-      results(i) = arma::mean(x.elem(ind));
-   }
-   return results;
+ // [[Rcpp::export()]]
+ arma::vec tapply_cpp(const arma::vec x, const arma::vec grp){
+ arma::vec grps = arma::unique(grp);
+ int d = grps.n_elem;
+ arma::vec results(d);
+
+ for(int i = 0; i < d; i++){
+ double current_grp = grps(i);
+ arma::uvec ind = arma::find(grp == current_grp);
+ results(i) = arma::mean(x.elem(ind));
+ }
+ return results;
 }
