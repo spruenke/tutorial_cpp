63727c75092d2769876a1df904c70ecadef5b5dc
Erin Spruenken <48289831+spruenke@users.noreply.github.com>
2025-03-28 13:28:23 +0100

    Add files via upload

--- Task III/ex3.cpp
+++ Task III/ex3.cpp
@@ -1,19 +1,25 @@
-#include<RcppArmadillo.h>
+#include <RcppArmadillo.h>
+
 // [[Rcpp::depends(RcppArmadillo)]]
 
-arma::mat data_gen(const arma::mat X, const arma::vec beta){
-      arma::vec epsilon = arma::randn(X.n_rows); 
-      arma::mat y = epsilon + X * beta + epsilon;
-      return y;
+arma::vec data_gen(const arma::mat X, const arma::vec beta) {
+  arma::vec epsilon = arma::randn(X.n_rows);
+  return X * beta + epsilon;
 }
 
-// insert the beta_est function from before at this place
+arma::vec beta_est(const arma::mat X, const arma::vec y) {
+  arma::mat xx = X.t() * X; // instead of X.t() arma::trans(X) also works
+  arma::mat xy = X.t() * y;
+  arma::vec beta_hat = arma::inv_sympd(xx) * xy;
+  return beta_hat;
+}
 
 // [[Rcpp::export()]]
-arma::mat sim_reg(const arma::mat X, const arma::vec beta, const int n_sim = 1e4){
-      arma::mat betas(X.n_cols, n_sim);
-      for(int i = 0; i < n_sim; i++){
-         betas.col(i) = beta_est(X, data_gen(X, beta));
-      }
-      return betas;
+arma::mat sim_reg(const arma::mat X, const arma::vec beta, const int n_sim = 10000) {
+  arma::mat betas(X.n_cols, n_sim);
+  for(int i = 0; i < n_sim; i++) {
+    betas.col(i) = beta_est(X, data_gen(X, beta));
+  }
+  
+  return betas;
 }
