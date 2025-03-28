3fb4a20e22bed4d74ceebe47c1df837e806d7006
Erin Spruenken <48289831+spruenke@users.noreply.github.com>
2025-03-28 13:27:33 +0100

    Add files via upload

--- Task II/ex2.cpp
+++ Task II/ex2.cpp
@@ -1,10 +1,11 @@
-#include<RcppArmadillo.h>
+#include <RcppArmadillo.h>
+
 // [[Rcpp::depends(RcppArmadillo)]]
 
 // [[Rcpp::export()]]
-arma::vec beta_est(const arma::mat X, const arma::vec y){
-   arma::mat xx = X.t() * X;
-   arma::mat xy = X.t() * y;
-   arma::vec beta_hat = inv_sympd(xx) * xy; // inv(xx) should work, too
-   return beta_hat;
+arma::vec beta_est(const arma::mat X, const arma::vec y) {
+  arma::mat xx = X.t() * X; // instead of X.t() arma::trans(X) also works
+  arma::mat xy = X.t() * y;
+  arma::vec beta_hat = arma::inv_sympd(xx) * xy;
+  return beta_hat;
 }
